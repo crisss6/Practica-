a10490d86e74aedf336137b5def6b270a3728a43
ASUS <ASUS@crisss6>
2019-12-16 07:56:00 -0500

    que mas mijin :v

---
+++ src/Semana1/semana.java
@@ -0,0 +1,13 @@
+
+package Semana1;
+
+
+public class semana {
+    public static void main(String[] args) {
+        semana1 s=new semana1();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+        
+    }
+    
+}
